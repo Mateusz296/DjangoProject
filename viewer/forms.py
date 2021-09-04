5501f60ca7e293a71d894cd3c757562970b67a2e
Mateusz <mateuszbusz4@wp.pl>
2021-09-04 11:44:26 +0200

    Initial commit

---
+++ viewer/forms.py
@@ -0,0 +1,41 @@
+from django.forms import (
+    ModelForm, CharField, IntegerField,
+)
+from viewer.models import Movie
+from viewer.validators import PastMonthField, capitalized_validator
+
+from django.core.exceptions import ValidationError
+
+import re
+
+class MovieForm(ModelForm):
+    class Meta: #subklasa opisująca dane z których będzie tworzony form
+        model = Movie #model na podstawie tworzymy formularz
+        fields = '__all__' #wykorzystujemy wszystkie pola z modelu
+
+    #pola z własnymi walidatorami dodajemy oddzielnie poza META
+    title = CharField(validators=[capitalized_validator])
+    rating = IntegerField(min_value=1, max_value=10)
+    released = PastMonthField()
+
+    def clean_description(self):
+        # pobranie wartości pola description
+        initial = self.cleaned_data['description']
+        # podział teksu na części "od kropki do kropki" - na zdania
+        sentences = re.sub(r'\s*\.\s*', '.', initial).split('.')
+        # zamiana na wielką literę pierwszej litery każdego ze zdań,
+        # dodanie kropki, powtórzenie operacji dla kolejnego zdania
+        return '. '.join(sentence.capitalize() for sentence in sentences)
+
+
+    def clean(self):
+        result = super().clean()
+        if result['genre'].name == 'comedy' and result['rating'] > 7:
+            # oznaczenie pola jako błędne bez komentarza
+            self.add_error('genre', '')
+            self.add_error('rating', '')
+            # rzucamy ogólny błąd / wyjątek
+            raise ValidationError(
+                'Commedies aren\'t so good to be over 7'
+            )
+        return result
---
+++ viewer/views.py
@@ -0,0 +1,51 @@
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
+from viewer.models import Movie
+from viewer.forms import MovieForm
+
+from logging import getLogger
+LOGGER = getLogger()
+
+
+class MoviesView(ListView):
+    template_name = 'movies.html'
+    model = Movie
+
+
+class MovieCreateView(CreateView):
+    template_name = 'form.html'
+    form_class = MovieForm
+    # adres pobrany z URLs na który zostaniemy przekierowani
+    # gdy walidacja się powiedzie (movie_create pochodzi z name!)
+    success_url = reverse_lazy('movie_create')
+
+    # co ma się dziać, gdy formularz nie przejdzie walidacji:
+    def form_invalid(self, form):
+        # odkładamy w logach informacje o operacji
+        LOGGER.warning('User provided invalid data')
+        # zwracamy wynik działania pierwotnej funkcji form_invalid
+        return super().form_invalid(form)
+
+class MovieUpdateView(UpdateView):
+    template_name = 'form.html'
+    form_class = MovieForm
+    # adres pobrany z URLs na który zostaniemy przekierowani
+    # gdy aktualizacja się powiedzie (index pochodzi z name!)
+    success_url = reverse_lazy('index')
+    # Nazwa encji, z której będziemy aktualizować rekord
+    model = Movie
+
+    # co ma się dziać, gdy formularz nie przejdzie walidacji:
+    def form_invalid(self, form):
+        # odkładamy w logach informacje o operacji
+        LOGGER.warning('User provided invalid data when updating')
+        # zwracamy wynik działania pierwotnej funkcji form_invalid
+        return super().form_invalid(form)
+
+class MovieDeleteView(DeleteView):
+    #Nazwa szablonu wraz z rozszerzeniem którą pobieramy z folderu templates
+    template_name = 'delete_movie.html'
+    success_url = reverse_lazy('index')
+    #Nazwa encji, z której będziemy kasować rekord
+    model = Movie

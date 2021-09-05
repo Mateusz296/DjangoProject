8a7934aa0be1449238eb42792592833d558d31a5
Mateusz <mateuszbusz4@wp.pl>
2021-09-05 09:40:53 +0200

    Custom Adding / Editing form label

--- viewer/views.py
+++ viewer/views.py
@@ -1,11 +1,24 @@
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
-
+from django.shortcuts import render
+import datetime
 from viewer.models import Movie
 from viewer.forms import MovieForm
 
 from logging import getLogger
 LOGGER = getLogger()
+
+from django.contrib.auth.decorators import login_required
+@login_required
+def generate_demo(request):
+    our_get = request.GET.get('name', '')
+    return render(
+        request, template_name='demo.html',
+        context={'our_get': our_get,
+                 'list': ['pierwszy', 'drugi', 'trzeci', 'czwarty'],
+                 'nasza_data': datetime.datetime.now()
+                 }
+    )
 
 
 class MoviesView(ListView):
